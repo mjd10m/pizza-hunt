642dfba0f01ca73c1345f4ce6715cc8b4e6f4825
mjd10m <mjd10m@outlook.com>
2022-11-06 15:06:29 -0500

    comment model routes and frontend

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,6 +1,8 @@
 const { Schema, model } = require('mongoose')
+const dateFormat = require('../utils/dateFormat')
 
 const PizzaSchema = new Schema({
+    
     pizzaName: {
         type: String
     },
@@ -9,13 +11,33 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: String,
-        default: Date.now
+        default: Date,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+    
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters:true
+    },
+    id: false
+}
+)
+
+PizzaSchema.virtual('commentCount').get(function(){
+    return this.comments.length
 })
 
 const Pizza = model('Pizza', PizzaSchema)
